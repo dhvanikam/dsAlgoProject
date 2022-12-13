9baedf1fc59cd39d3983d9b04dbbed8381b3bc24
sathyavinoth02 <sathyavinoth12@gmail.com>
2022-12-13 11:30:27 -0500

    latest

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -1,15 +1,10 @@
 package pageObjects;
 
-import java.time.Duration;
-
-import org.openqa.selenium.By;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import driverFactory.DriverFactory;
@@ -20,12 +15,13 @@ public class HomePage {
 	public  static WebDriver driver;
 	String URL=ConfigReader.getApplicationUrl();
 	@FindBy (xpath="//a[@href=\"/home\"]")WebElement getstart_link;
+	@FindBy (xpath="//a[contains(text(),'Get Started')]")WebElement getstart;
+	@FindBy (xpath="//*[@class=\"alert alert-primary\"]")WebElement alert_msg;
 	
 	
 	@Test
 	public void homepage() throws Exception {
 		
-		Loggerload.info("User is on DSAlgo portal page");
 		try {
 			
 			driver=DriverFactory.getdriver();
@@ -42,15 +38,22 @@ public class HomePage {
 
 	public void getStarted_link(String str) {
 		
-		Loggerload.info("User clicks on "+str+" Link");
 		getstart_link.click();
 		
 	}
 
 	public String getPageTitle() {
-		Loggerload.info("Get the Title of the Page:");
 		String title=driver.getTitle();
 		return title;
+		
+	}
+	
+	public String alert_login() {
+		
+		getstart.click();
+		String msg=alert_msg.getText();
+		Loggerload.warn(msg);
+		return msg;
 		
 	}
 	
--- src/test/resources/features/Home.feature
+++ src/test/resources/features/Home.feature
@@ -1,9 +1,10 @@
-@Home
 Feature: User launch dsalgo application
-  @tag1
+
   Scenario: User launch home page of an dsalgo project
     Given The user opens DS Algo portal link
     When The user clicks the "Get Started" button
     Then The user should be redirected to homepage
     
+    When The user clicks on "Get Started" link on homepage without login
+    Then The user warnin message " You are not logged in"
    
