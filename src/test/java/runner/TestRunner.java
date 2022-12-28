16cc442fbef0c43c34a235cca34b2bc3f5a83d13
Dhvanika <dhvanika.patel@gmail.com>
2022-12-28 18:08:04 -0500

    updated hooks file for log,runner file, clean up utils file

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,10 +5,10 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(plugin = { "pretty", "html:target/TS_001.html" }, // reporting purpose
+@CucumberOptions(plugin = { "pretty", "html:target/dsAlgoReport.html" }, // reporting purpose
 		monochrome = false, // console output
 		tags = "", // tags from feature file
-		features = { "src/test/resources/features/Home.feature"}, // location of feature files
+		features = { "src/test/resources/features"}, // location of feature files
 		glue = {"stepDefinition", "appHooks"}) // location of step definition files
 		
 public class TestRunner extends AbstractTestNGCucumberTests {
